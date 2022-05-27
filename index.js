26e265c938dd26e04dd03f1c7fc9381efe9f88ba
iamyeasirarafat <yas.yeasir@gmail.com>
2022-05-27 07:35:21 +0600

    mongodb connected and get products

--- index.js
+++ index.js
@@ -11,7 +11,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+ //mongoDb Database connection
+ const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.zzc8v.mongodb.net/?retryWrites=true&w=majority`;
+ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ const run = async () => {
+     try{
+         await client.connect();
+         const productsDb = client.db("irwinTools").collection('productsDb')
 
+        // get all the products
+        app.get('/', async (req, res) => {
+            const products = await productsDb.find().toArray()
+            res.send(products)
+        })
+
+     }
+     finally{}
+ }
+run().catch(console.dir);
 
 app.listen(port , ()=>{
     console.log('listening to the port', port);
